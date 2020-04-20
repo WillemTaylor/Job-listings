fbc225a7423a79e4cfa5643e8936a7455a4f7e83
WillemTaylor <willemtaylor@msn.com>
2020-04-20 13:25:44 +0100

    keywords filter working. Displaying tiles WIP

--- src/App.vue
+++ src/App.vue
@@ -3,50 +3,52 @@
     <Header />
     <div v-if="filter" class="tile keywords">
       <div v-if="showFilterDashboard" class="filters">
-        <div v-if="role">
+        <div v-if="!removeRoleClicked && role">
           <h4>{{ role }}</h4>
-          <div @click="handleRemoveFilter">&#10006;</div>
+          <div @click="handleRole">&#10006;</div>
         </div>
-        <div v-if="level">
+        <div v-if="!removeLevelClicked && level">
           <h4>{{ level }}</h4>
-          <div @click="handleRemoveFilter">&#10006;</div>
+          <div @click="handleLevel">&#10006;</div>
         </div>
       </div>
       <h5 @click="removeFilterDashboard">Clear</h5>
     </div>
-    <div class="tile" v-for="tile in data" :key="tile.data">
-      <div class="info-container">
-        <div>
-          <img :src="require(`${tile.logo}`)" />
-        </div>
-        <div>
-          <h4>{{ tile.company }}</h4>
-          <div v-if="tile.new" class="new-tag">
-            <p>NEW!</p>
+    <div v-if="!role && !level">
+      <div class="tile" v-for="tile in data" :key="tile.data">
+        <div class="info-container">
+          <div>
+            <img :src="require(`${tile.logo}`)" />
           </div>
-          <div v-if="tile.featured" class="featured-tag">
-            <p>FEATURED</p>
+          <div>
+            <h4>{{ tile.company }}</h4>
+            <div v-if="tile.new" class="new-tag">
+              <p>NEW!</p>
+            </div>
+            <div v-if="tile.featured" class="featured-tag">
+              <p>FEATURED</p>
+            </div>
+            <h3>{{ tile.position }}</h3>
+            <p>
+              {{ tile.postedAt }} &bull; {{ tile.contract }} &bull;
+              {{ tile.location }}
+            </p>
+            <hr class="desktop-hide" />
           </div>
-          <h3>{{ tile.position }}</h3>
-          <p>
-            {{ tile.postedAt }} &bull; {{ tile.contract }} &bull;
-            {{ tile.location }}
-          </p>
-          <hr class="desktop-hide" />
         </div>
-      </div>
-      <div class="filters">
-        <div>
-          <h4 @click="handleRoleFilter">{{ tile.role }}</h4>
-        </div>
-        <div>
-          <h4 @click="handleLevelFilter">{{ tile.level }}</h4>
-        </div>
-        <div v-for="lang in tile.languages" :key="lang.languages">
-          <h4>{{ lang }}</h4>
-        </div>
-        <div v-for="tool in tile.tools" :key="tool.tools">
-          <h4>{{ tool }}</h4>
+        <div class="filters">
+          <div>
+            <h4 @click="handleRoleFilter">{{ tile.role }}</h4>
+          </div>
+          <div>
+            <h4 @click="handleLevelFilter">{{ tile.level }}</h4>
+          </div>
+          <div v-for="lang in tile.languages" :key="lang.languages">
+            <h4>{{ lang }}</h4>
+          </div>
+          <div v-for="tool in tile.tools" :key="tool.tools">
+            <h4>{{ tool }}</h4>
+          </div>
         </div>
       </div>
     </div>
@@ -67,14 +69,11 @@ export default {
       data: json,
       filter: false,
       showFilterDashboard: true,
+      removeRoleClicked: false,
+      removeLevelClicked: false,
       role: "",
       level: ""
     };
-  },
-  mounted() {
-    if (this.roleFilter === "" && this.levelFilter === "") {
-      this.showFilterDashboard = false;
-    }
   },
   methods: {
     removeFilterDashboard() {
@@ -82,15 +81,26 @@ export default {
       this.level = "";
       this.filter = false;
     },
-    handleRemoveFilter() {
-      event.target.parentElement.remove();
+    handleRole() {
+      this.removeRoleClicked = true;
+      if (this.removeRoleClicked && this.removeLevelClicked) {
+        this.removeFilterDashboard();
+      }
+    },
+    handleLevel() {
+      this.removeLevelClicked = true;
+      if (this.removeRoleClicked && this.removeLevelClicked) {
+        this.removeFilterDashboard();
+      }
     },
     handleRoleFilter() {
       this.role = event.target.innerHTML;
+      this.removeRoleClicked = false;
       this.filter = true;
     },
     handleLevelFilter() {
       this.level = event.target.innerHTML;
+      this.removeLevelClicked = false;
       this.filter = true;
     }
   }
